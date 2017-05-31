b53c9718b3fd483f989da3cf2631a7845d50f913
Lucas Albuquerque de Almeida Andrade <lucas.almeida@ccc.ufcg.edu.br>
2017-05-31 17:29:32 -0300

    WIP - Tela de cadastro | adicionando validacao de campos

--- app/src/main/java/icm/censo/a3_code/com/censoicm/CadastroActivity.java
+++ app/src/main/java/icm/censo/a3_code/com/censoicm/CadastroActivity.java
@@ -1,22 +1,35 @@
 package icm.censo.a3_code.com.censoicm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
 
 import model.Censo;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private Censo censo;
     private int varoesValue, senhorasValue, jovensValue, adolescentesValue, criancasValue, visitantesValue, totalValue;
     private TextView totalTextView;
+    //Campos de frequencia
     private EditText varoesField, senhorasField, jovensField, adolescentesField, criancasField, visitantesField;
+    //Campos de informacoes textuais
+    private EditText portaField, palavraField, louvorField, domField;
+
+    private Button botaoSalvar;
 
     /**
      * Carrega os valores dos campos editText para as variaveis correspondentes.
@@ -141,13 +154,57 @@ public class CadastroActivity extends AppCompatActivity {
         });
     }
 
+    private Censo buildCenso(){
+        Censo censo = new Censo();
+
+        censo.setQtdVaroes(varoesValue);
+        censo.setQtdSenhoras(senhorasValue);
+        censo.setQtdJovens(jovensValue);
+        censo.setQtdCriancas(criancasValue);
+        censo.setQtdVisitantes(visitantesValue);
+        censo.setQtdAdolescentes(adolescentesValue);
+        censo.setTotalPessoas(totalValue);
+
+        String palavra = palavraField.getText().toString();
+        String louvor = louvorField.getText().toString();
+        String dom  = domField.getText().toString();
+        String[] porta = portaField.getText().toString().split(" ");
+
+        censo.setObreiroPalavra(palavra);
+        censo.setObreiroLouvor(louvor);
+        censo.setObreirosPorta(new HashSet<String>(Arrays.asList(porta)));
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        censo.setData(new Date());
+
+        return censo;
+    }
+
+    /**
+     * Verifica se todos os campos obrigatorios foram preenchidos
+     * @return boolean
+     */
+    public boolean verificaCamposObrigatorios(){
+        boolean valido = true;
+
+        if(palavraField.getText().toString().isEmpty()){
+            palavraField.setHintTextColor(Color.RED);
+            valido = false;
+        }if(louvorField.getText().toString().isEmpty()){
+            louvorField.setHintTextColor(Color.RED);
+            valido = false;
+        }if(totalValue <= 0){
+            totalTextView.setTextColor(Color.RED);
+            valido = false;
+        }
+
+        return  valido;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
-
-        //Inicializando objeto censo
-        censo = new Censo();
 
         //Carregando valores dos campos de edicao
         varoesField = (EditText) findViewById(R.id.varoes);
@@ -157,8 +214,26 @@ public class CadastroActivity extends AppCompatActivity {
         criancasField = (EditText) findViewById(R.id.criancas);
         visitantesField = (EditText) findViewById(R.id.visitantes);
         totalTextView = (TextView) findViewById(R.id.total);
+        portaField = (EditText) findViewById(R.id.porta);
+        palavraField = (EditText) findViewById(R.id.palavra);
+        louvorField = (EditText) findViewById(R.id.louvor);
+        domField = (EditText) findViewById(R.id.dom);
+        botaoSalvar = (Button) findViewById(R.id.salvar);
 
         addTextChangedListener();
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(verificaCamposObrigatorios()){
+                    buildCenso();
+                    Toast.makeText(getApplicationContext(), "Dados Salvos", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Preencha os campos obrigatórios", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
