1c3924ec27a49b5ba8c78b3b74a21a01d69adff8
Lucas Albuquerque de Almeida Andrade <lucas.almeida@ccc.ufcg.edu.br>
2017-06-20 17:07:18 -0300

    WIP: geracao de relatorio xls

--- app/src/main/java/icm/censo/a3_code/com/censoicm/RelatorioMesActivity.java
+++ app/src/main/java/icm/censo/a3_code/com/censoicm/RelatorioMesActivity.java
@@ -5,15 +5,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -40,6 +37,7 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -47,7 +45,17 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import controller.CensoController;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.format.Colour;
+import jxl.write.Label;
+import jxl.write.Number;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 import model.Censo;
 import util.DBEsquema;
 import util.EstatisticaMes;
@@ -209,7 +217,7 @@ public class RelatorioMesActivity extends AppCompatActivity {
         pieChart.invalidate();
     }
 
-    private void takeScreenshot(final ScrollView view, final Button baixarBtn) {
+    private void takeScreenshot(final ScrollView view, final Button baixarBtn, final Button gerarExcellBtn) {
         Date now = new Date();
         android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
 
@@ -220,6 +228,7 @@ public class RelatorioMesActivity extends AppCompatActivity {
 
             //Don't show the button
             baixarBtn.setVisibility(View.GONE);
+            gerarExcellBtn.setVisibility(View.GONE);
 
             Bitmap bitmap = Bitmap.createBitmap(
                     view.getChildAt(0).getWidth(),
@@ -243,6 +252,7 @@ public class RelatorioMesActivity extends AppCompatActivity {
             view.startAnimation(animation);
 
             baixarBtn.setVisibility(View.VISIBLE);
+            gerarExcellBtn.setVisibility(View.VISIBLE);
             Toast.makeText(getApplicationContext(), "Relatório salvo em suas imagens.", Toast.LENGTH_SHORT).show();
         } catch (Throwable e) {
             // Several error may come out with file handling or OOM
@@ -270,10 +280,19 @@ public class RelatorioMesActivity extends AppCompatActivity {
 
             final ScrollView scrollView = (ScrollView) findViewById(R.id.relatorio_geral_mes);
             final Button baixarBtn = (Button) findViewById(R.id.baixarRelatorioMes);
+            final Button gerarExcellBtn = (Button) findViewById(R.id.gerarRelatorioExcell);
+
             baixarBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    takeScreenshot(scrollView, baixarBtn);
+                    takeScreenshot(scrollView, baixarBtn, gerarExcellBtn);
+                }
+            });
+
+            gerarExcellBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    gerarRelatorioExcell();
                 }
             });
 
@@ -335,5 +354,183 @@ public class RelatorioMesActivity extends AppCompatActivity {
         estatisticaMes.setDataInicio(list.get(totalDiasRegistrados-1).getData());
         estatisticaMes.setDataFim(list.get(0).getData());
         return estatisticaMes;
+    }
+
+    /**
+     * Gera o excell com as informações de vários objestos censo
+     */
+    private void gerarRelatorioExcell(){
+        String fileName = "relatorio_censo_icm_"+Calendar.getInstance().getTimeInMillis()+".xls";
+        WorkbookSettings workbookSettings = new WorkbookSettings();
+        workbookSettings.setUseTemporaryFileDuringWrite(true);
+
+        String path = Environment.getExternalStorageDirectory().toString() +"/"+ Environment.DIRECTORY_DOWNLOADS.toString()+"/"+fileName;
+        //add on the your app's path
+        File dir = new File(path);
+        //make them in case they're not there
+        dir.mkdirs();
+
+        //create a standard java.io.File object for the Workbook to use
+        File wbfile = new File(path,fileName);
+
+        Log.i("Excel Path:",wbfile.getPath());
+
+        WritableWorkbook writableWorkbook = null;
+
+        try{
+            //create a new WritableWorkbook using the java.io.File and
+            //WorkbookSettings from above
+            writableWorkbook = Workbook.createWorkbook(wbfile,workbookSettings);
+
+            // create an Excel sheet
+            WritableSheet excelSheet = writableWorkbook.createSheet("Censo Icm", 0);
+
+            //Formt Cell
+            WritableCellFormat cellFormat = new WritableCellFormat();
+            WritableFont font = new WritableFont(WritableFont.ARIAL, 14, WritableFont.BOLD);
+            font.setColour(Colour.WHITE);
+            cellFormat.setBackground(Colour.LIGHT_BLUE);
+            cellFormat.setFont(font);
+
+            int colunaDataIndex = 0;
+            int colunaLouvoresIndex = 1;
+            int colunaRecpcaoIndex = 2;
+            int colunaObreiroLouvorIndex = 3;
+            int colunaObreiroPalavraIndex = 4;
+            int colunacolunaTxtBiblicoIndex = 5;
+            int colunaVaroesIndex = 6;
+            int colunaSenhorasIndex = 7;
+            int colunaJovensIndex = 8;
+            int colunaAdolescentesIndex = 9;
+            int colunaCriancasIndex = 10;
+            int colunaVisitantesIndex = 11;
+            int colunaTotalIndex = 12;
+            int colunaDomIndex = 13;
+
+
+            Label colunaData = new Label(colunaDataIndex,0,"Data",cellFormat);
+            excelSheet.addCell(colunaData);
+
+            Label colunaLouvores = new Label(colunaLouvoresIndex,0,"Lista de Louvores",cellFormat);
+            excelSheet.addCell(colunaLouvores);
+
+            Label colunaRecpcao = new Label(colunaRecpcaoIndex,0,"Recpção/Preparo",cellFormat);
+            excelSheet.addCell(colunaRecpcao);
+
+            Label colunaObreiroLouvor = new Label(colunaObreiroLouvorIndex,0,"Servo(a) No Louvor",cellFormat);
+            excelSheet.addCell(colunaObreiroLouvor);
+
+            Label colunaObreiroPalavra = new Label(colunaObreiroPalavraIndex,0,"Servo(a) Na Palavra",cellFormat);
+            excelSheet.addCell(colunaObreiroPalavra);
+
+            Label colunaTxtBiblico = new Label(colunacolunaTxtBiblicoIndex,0,"Texto Bíblico",cellFormat);
+            excelSheet.addCell(colunaTxtBiblico);
+
+            Label colunaVaroes = new Label(colunaVaroesIndex,0,"N° Varões",cellFormat);
+            excelSheet.addCell(colunaVaroes);
+
+            Label colunaSenhoras = new Label(colunaSenhorasIndex,0,"N° Senhoras",cellFormat);
+            excelSheet.addCell(colunaSenhoras);
+
+            Label colunaJovens = new Label(colunaJovensIndex,0,"N° Jovens",cellFormat);
+            excelSheet.addCell(colunaJovens);
+
+            Label colunaAdolescentes = new Label(colunaAdolescentesIndex,0,"N° Adolescentes",cellFormat);
+            excelSheet.addCell(colunaAdolescentes);
+
+            Label colunaCriancas = new Label(colunaCriancasIndex,0,"N° Crianças",cellFormat);
+            excelSheet.addCell(colunaCriancas);
+
+            Label colunaVisitantes = new Label(colunaVisitantesIndex,0,"N° Visitantes",cellFormat);
+            excelSheet.addCell(colunaVisitantes);
+
+            Label colunaTotal = new Label(colunaTotalIndex,0,"N° Total",cellFormat);
+            excelSheet.addCell(colunaTotal);
+
+            Label colunaDom = new Label(colunaDomIndex,0,"Dom",cellFormat);
+            excelSheet.addCell(colunaDom);
+
+            populaExcell(censoList, excelSheet);
+
+            writableWorkbook.write();
+            writableWorkbook.close();
+
+            Toast.makeText(getApplicationContext(), "Relatório Gerado com Sucesso.", Toast.LENGTH_SHORT).show();
+
+        }catch(Exception ex){
+            Log.e("EXCELL",ex.getStackTrace().toString());
+            Log.e("EXCELL", ex.getMessage());
+            Toast.makeText(getApplicationContext(),getString(R.string.erro_generico), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private  void populaExcell(List<Censo> censoList, WritableSheet sheet) throws WriteException {
+        int colunaDataIndex = 0;
+        int colunaLouvoresIndex = 1;
+        int colunaRecpcaoIndex = 2;
+        int colunaObreiroLouvorIndex = 3;
+        int colunaObreiroPalavraIndex = 4;
+        int colunacolunaTxtBiblicoIndex = 5;
+        int colunaVaroesIndex = 6;
+        int colunaSenhorasIndex = 7;
+        int colunaJovensIndex = 8;
+        int colunaAdolescentesIndex = 9;
+        int colunaCriancasIndex = 10;
+        int colunaVisitantesIndex = 11;
+        int colunaTotalIndex = 12;
+        int colunaDomIndex = 13;
+
+        if(sheet != null && censoList != null && censoList.size() > 0){
+            Locale BRAZIL = new Locale("pt", "BR");
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", BRAZIL);
+
+            for (int i=0;i<censoList.size();i++){
+                Censo censo = censoList.get(i);
+                int linha = i+1;
+                Label dataValue = new Label(colunaDataIndex, linha,sdf.format(censo.getData()).toString());
+                sheet.addCell(dataValue);
+
+                String louvores = censo.getLouvores().toString().replace("[","").replace("]","");
+                Label louvoresValues = new Label(colunaLouvoresIndex, linha, louvores);
+                sheet.addCell(louvoresValues);
+
+                String obreirosPorta = censo.getObreirosPorta().toString().replace("[","").replace("]","");
+                Label obreirosPortaValue = new Label(colunaRecpcaoIndex, linha,obreirosPorta);
+                sheet.addCell(obreirosPortaValue);
+
+                Label obreiroLouvorValue = new Label(colunaObreiroLouvorIndex, linha, censo.getObreiroLouvor());
+                sheet.addCell(obreiroLouvorValue);
+
+                Label obreiroPalavraValue = new Label(colunaObreiroPalavraIndex, linha, censo.getObreiroPalavra());
+                sheet.addCell(obreiroPalavraValue);
+
+                Label txtBiblicoValue = new Label(colunacolunaTxtBiblicoIndex,linha,censo.getTextoBiblico());
+                sheet.addCell(txtBiblicoValue);
+
+                Number vaoresValue = new Number(colunaVaroesIndex,linha,censo.getQtdVaroes());
+                sheet.addCell(vaoresValue);
+
+                Number senhorasValue = new Number(colunaSenhorasIndex,linha,censo.getQtdSenhoras());
+                sheet.addCell(senhorasValue);
+
+                Number jovensValue = new Number(colunaJovensIndex, linha, censo.getQtdJovens());
+                sheet.addCell(jovensValue);
+
+                Number adolescentesValue = new Number(colunaAdolescentesIndex, linha, censo.getQtdAdolescentes());
+                sheet.addCell(adolescentesValue);
+
+                Number criancasValue = new Number(colunaCriancasIndex, linha, censo.getQtdCriancas());
+                sheet.addCell(criancasValue);
+
+                Number visitantesValue = new Number(colunaVisitantesIndex, linha, censo.getQtdVisitantes());
+                sheet.addCell(visitantesValue);
+
+                Number totalValue = new Number(colunaTotalIndex, linha, censo.getTotalPessoas());
+                sheet.addCell(totalValue);
+
+                Label domValue = new Label(colunaDomIndex, linha, censo.getDom());
+                sheet.addCell(domValue);
+            }
+        }
     }
 }
